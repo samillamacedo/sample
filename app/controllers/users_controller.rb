b817a16d739cd7c7d0c4e5165f81514456bc24dc
Samilla Macedo <samilla.macedo@capim.com.br>
2022-03-22 04:28:40 -0300

    Remove unused actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,9 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[ show edit update destroy ]
+  before_action :set_user, only: %i[ edit update destroy ]
 
   # GET /users or /users.json
   def index
     @users = User.all
-  end
-
-  # GET /users/1 or /users/1.json
-  def show
   end
 
   # GET /users/new
@@ -25,8 +21,8 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to user_url(@user), notice: "User was successfully created." }
-        format.json { render :show, status: :created, location: @user }
+        format.html { redirect_to users_path, notice: "Usuário foi criado com sucesso" }
+        format.json { render :index, status: :created, location: @user }
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @user.errors, status: :unprocessable_entity }
@@ -38,8 +34,8 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to user_url(@user), notice: "User was successfully updated." }
-        format.json { render :show, status: :ok, location: @user }
+        format.html { redirect_to users_path, notice: "Usuário foi editado com sucesso" }
+        format.json { render :index, status: :ok, location: @user }
       else
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @user.errors, status: :unprocessable_entity }
